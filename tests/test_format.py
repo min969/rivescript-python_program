ca04f9f24d6346ba7155bb46b766f435bf25c3d9
Aung Si Thu MinZin <73772552+min969@users.noreply.github.com>
2021-11-24 17:19:54 +0700

    Initialize docker

---
+++ tests/test_format.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, absolute_import
+
+from .config import RiveScriptTestCase
+
+class MessageFormatTests(RiveScriptTestCase):
+    """Test format message."""
+    def test_format_message(self):
+        self.new("""
+            + hello bot
+            - hello human
+        """)
+        self.reply("hello bot", "hello human")
+        self.reply("Hello Bot", "hello human")
+        self.reply("  hello bot   ", "hello human") # Strip leading and trailing whitespaces
+        self.reply("  hello   bot   ", "hello human") # Replace the multiple whitespaces by single whitespace
+        self.reply("hello      bot!!!???   ", "hello human") # Strip nasties
+
+    def test_format_triggers(self):
+        self.new("""
+            + hi there
+            -    hi   there
+
+            +hi  here
+            -hi  here
+
+        """)
+        self.reply("hi there", "hi there")
+        self.reply("hi  here", "hi here")
+
+    def test_check_syntax(self):
+        mismatch_brackets = ["a (b", "a [b", "a {b", "a <b", "a b)", "a b]", "a b}", "a b>"]
+        empty_pipes = ["[a|b| ]", "[a|b|]", "[a| |c]", "[a||c]", "[ |b|c]", "[|b|c]"]
+        advanced_brackets = [") a (", "] b [", "> c <", "} d {", "a (b [c) d]", "a (b [c|d] e)"]
+        angle_brackets = ["(a <b) c>", "<a (b > c)", "[a <b ] c>", "< a [b > c]", "{ a < b } c >", "< a {b > c }"]
+        pipe_outside = ["a|b", "a|", "|b", "(a|b) | (c|d)", "(a|b)|(c|d)"]
+
+        for failing_trigger in mismatch_brackets + empty_pipes + advanced_brackets + pipe_outside + angle_brackets:
+            self.assertRaises(Exception, self.new, """
+                + {}
+                - hi
+            """.format(failing_trigger))
+
+        self.new("""
+            ! version = 2.0
+
+            // Bot variables
+            ! var name = Tutorial
+            ! var nickname = tut
+
+            + [<bot name>|<nickname>] *
+            - You called?
+        """)
+        self.reply("Tutorial", "You called?")
+        self.reply("tut", "You called?")
+
+    def test_invalid_character_raise_exception(self):
+        self.assertRaises(Exception, self.new, """
+            + $hello
+            - hi
+        """)  # This test passes with `match`, which only check at the beginning
+        self.assertRaises(Exception, self.new, """
+            + hello$
+            - hi
+        """)   # This test does not pass because the beginning is good, no $
+        self.assertRaises(Exception, self.new, """
+            > topic Greetings
+                + hello
+                - hi
+            <topics
+        """)
+        self.assertRaises(Exception, self.new, """
+            > object hash %perl
+                my ($rs, $args) = @_;
+                my $method = shift @{$args};
+            <object
+        """)  # Test for character violation in object, no %
+        self.new("""
+            > object hash Perl
+                my ($rs, $args) = @_;
+                my $method = shift @{$args};
+            <object
+        """) # No exception raised for uppercase character in object
+
+    def test_space_tolerance_with_pipe(self):
+        self.new("""
+                    + hey [ a | b|c ]
+                    - hi
+                """)
+        for message in ['hey a', 'hey b', 'hey c']:
+            self.reply(message, "hi")
---
+++ tests/test_sorting.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, absolute_import
+
+from rivescript.exceptions import RS_ERR_MATCH
+from .config import RiveScriptTestCase
+
+class SortingTriggersTest(RiveScriptTestCase):
+    """Topic tests."""
+
+    def test_sorting_triggers(self):
+        self.new("""
+            + * you *
+            - 1
+
+            + * are *
+            - 2
+
+            + * how are you *
+            - 3
+
+            + * hallo ween *
+            - 4
+
+            + (hi|hii)
+            - 5
+
+            + hello
+            - 6 
+
+            + hey [man]
+            - 7
+
+            + good morning
+            - 8
+
+            + *
+            - 9
+
+            + hel lo
+            - 10
+
+            + hi *
+            - 11
+
+            + hi [*]
+            - 12
+
+            + [*] hi [*]
+            - 13
+
+            + [*] hi *
+            - 14
+
+            + [*]
+            - 15
+
+            + hi _
+            - 16
+
+            + _ _
+            - 17
+
+            + ho _{weight=100}
+            - 18
+
+            + ho _
+            - 19
+        """)
+        
+        sorted_triggers =  {trig[0]:position for position, trig in enumerate(self.rs._brain.master._sorted["topics"]['random'])}
+
+        # 1) Atomic is first matched. 
+        self.assertLess(sorted_triggers['hello'], sorted_triggers['hey [man]'])
+
+        # 2) Sorted by number of words 
+        self.assertLess(sorted_triggers['hel lo'], sorted_triggers['hello'])
+        self.assertLess(sorted_triggers['* hallo ween *'], sorted_triggers['* are *'])
+
+        # 3) Sorted by length by characters 
+        self.assertLess(sorted_triggers['good morning'], sorted_triggers['hel lo'])
+
+        # 4) Sorted by alphabetical order
+        self.assertLess(sorted_triggers['* are *'], sorted_triggers['* you *'])
+
+        # 5) Sorted by number of wildcard triggers 
+        self.assertLess(sorted_triggers['hi *'], sorted_triggers['* you *'])
+
+        # 6) The `super catch all` (only single star `*` or `[*]`) should be the last two
+        third_last_position = max(sorted_triggers.values())-2
+        self.assertLess(third_last_position, sorted_triggers['*'])
+        self.assertLess(sorted_triggers['hi [*]'], sorted_triggers['*'])
+        self.assertLess(third_last_position, sorted_triggers['[*]'])
+        self.assertLess(sorted_triggers['[*] hi [*]'], sorted_triggers['[*]'])
+        self.assertLess(sorted_triggers['[*] hi *'], sorted_triggers['*'])
+        self.assertLess(sorted_triggers['hi [*]'], sorted_triggers['[*]'])
+
+        # 7) Trigger with no text should rank lower than trigger with some text, even with wildcards.
+        self.assertLess(sorted_triggers['hel lo'], sorted_triggers['_ _'])
+        self.assertLess(sorted_triggers['hi [*]'], sorted_triggers['_ _'])
+        self.assertLess(sorted_triggers['hi *'], sorted_triggers['_ _'])
+        self.assertLess(sorted_triggers['hi _'], sorted_triggers['_ _'])
+
+        # 8) Among the triggers with no text, the order of wildcard priority still holds
+        self.assertLess(sorted_triggers['_ _'], sorted_triggers['[*]'])
+        self.assertLess(sorted_triggers['_ _'], sorted_triggers['*'])
+
+        # 9) Among the triggers with text, the order of wildcard priority still holds
+        self.assertLess(sorted_triggers['hi _'], sorted_triggers['hi *'])
+        self.assertLess(sorted_triggers['hi _'], sorted_triggers['hi [*]'])
+
+        # 10) Among the triggers with text, the order of wildcard priority still holds
+        self.assertLess(sorted_triggers['hi _'], sorted_triggers['hi *'])
+        self.assertLess(sorted_triggers['hi _'], sorted_triggers['hi [*]'])
+
+        # 11) Making sure that the weight tag is taken into account
+        self.assertLess(sorted_triggers['ho _{weight=100}'], sorted_triggers['hi _'])
+        self.assertLess(sorted_triggers['hi _'], sorted_triggers['ho _'])
