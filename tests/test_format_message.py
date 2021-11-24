c54e117126e17497a18a962cca6637ef190d2d31
Aung Si Thu MinZin <73772552+min969@users.noreply.github.com>
2021-11-24 17:19:53 +0700

    Initialize feature/sessions

---
+++ tests/test_format_message.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, absolute_import
+
+from .config import RiveScriptTestCase
+
+class MessageFormatTests(RiveScriptTestCase):
+    """Test format message."""
+    def test_format_message(self):
+        self.new("""
+            + hello bot
+            - hello human
+        """)
+        self.reply("hello bot", "hello human")
+        self.reply("Hello Bot", "hello human")
+        self.reply("  hello bot   ", "hello human") # Strip leading and trailing whitespaces
+        self.reply("  hello   bot   ", "hello human") # Replace the multiple whitespaces by single whitespace
+        self.reply("hello      bot!!!???   ", "hello human") # Strip nasties
+
+    def test_format_triggers(self):
+        self.new("""
+            + hi there
+            -    hi   there
+
+            +hi  here
+            -hi  here
+
+        """)
+        self.reply("hi there", "hi there")
+        self.reply("hi  here", "hi here")
